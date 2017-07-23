dc457de0e9e4e6e1622647a7abb3b0bf82072fbf
Claire Johnson <c.eliseee@gmail.com>
2017-07-23 09:53:18 -0700

    Moved index.html script to index.js. Added timestamp function to index.js

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+     var $body = $('body');
+     // $body.html('');
+     var index = streams.home.length - 1;
+
+     while(index >= 0){
+       let tweet = streams.home[index];
+       let $tweet = $('<div class="tweet"></div>');
+       let $date = $('<div class="timestamp"></div>')
+       let date = getDate();
+       $date.text(date);
+       $tweet.text('@' + tweet.user + ': ' + tweet.message);
+       $tweet.appendTo($('.tweet-container'));
+       $date.appendTo($tweet);
+       index -= 1;
+     }
+
+   });
+
+function getDate() {
+  let today = new Date();
+  let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  return date + ' ' + time;
+}
