84b56bb2f4e84b40ba3de13ba2a7ad4c16b1a38e
Claire Johnson <c.eliseee@gmail.com>
2017-07-23 12:36:58 -0700

    Fixed date stamp to use tweet object's created_at key instead of creating a new date object. Began to create generateProfile function for viewing user profile, but function is not yet invoked in click listener.

--- script.js
+++ script.js
@@ -17,25 +17,53 @@ $(document).ready(function(){
       $('.profile-container').css('display', 'none');
     })
     $('.user').on('click', function() {
-      // generateProfile();
+      // generateProfile($(this).html());
     })
    });
 
-function getDate() {
-  let today = new Date();
-  let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  return date + ' ' + time;
-}
+// function getDate() {
+//   let today = new Date();
+//   let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+//   let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//   return date + ' ' + time;
+// }
 
 function postTweets(end) {
-  var index = streams.home.length - 1;
-  var newEnd = index + 1;
+  let index = streams.home.length - 1;
+  let newEnd = index + 1;
   while(index >= end){
     let tweet = streams.home[index];
     let $tweet = $('<div class="tweet"></div>');
     let $date = $('<div class="timestamp"></div>')
-    let date = getDate();
+    let date = tweet.created_at;
+    let $user = $('<span class="user"></span>')
+    $user.text('@' + tweet.user);
+    $date.text(date);
+    $tweet.text(': ' + tweet.message);
+    // $user.append($tweet).appendTo($('.tweet-container'));
+    $tweet.appendTo($('.tweet-container'));
+    $('.user').text('@' + tweet.user);
+    $tweet.append($date);
+    $tweet.prepend($user);
+    index -= 1;
+    console.log(tweet);
+  }
+  return newEnd;
+}
+
+function generateProfile(user) {
+  $('.profile-container').css('display', 'flex');
+  $('.tweet-container').css('display', 'none');
+  let username = user.match(/[^@]/gi).join('');
+  let tweets = streams.users[username];
+  console.log(tweets);
+
+  let index = streams.home.length - 1;
+  while(index >= end){
+    let tweet = streams.home[index];
+    let $tweet = $('<div class="tweet"></div>');
+    let $date = $('<div class="timestamp"></div>')
+    // let date = getDate();
     let $user = $('<span class="user"></span>')
     $user.text('@' + tweet.user);
     $date.text(date);
@@ -47,5 +75,5 @@ function postTweets(end) {
     $tweet.prepend($user);
     index -= 1;
   }
-  return newEnd;
+
 }
