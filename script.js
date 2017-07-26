fb5505cbc08596a9f2c947b1a7befb30a0f25dfa
Claire Johnson <c.eliseee@gmail.com>
2017-07-26 13:23:00 -0700

    Added moment.js library to allow for user-friendly dates (relative time). fixted script.js to update relative time when tweets post.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
       $('.user').on('click', function() {
         generateProfile($(this).html(), 0);
       });
+
     }, 20000);
 
     $('.title').on('click', function() {
@@ -19,23 +20,40 @@ $(document).ready(function(){
     });
    });
 
+function updateDates() {
+  let absTime, relTime;
+  let divs = document.getElementsByClassName('timestamp');
+  for (let i = 0; i < divs.length; i++) {
+    let cur = divs[i];
+    absTime = cur.getElementsByClassName('absolute')[0].innerText;
+    relTime = moment(Date.parse(absTime)).fromNow();
+    cur.childNodes[0].nodeValue = relTime;
+  }
+}
+
 function postTweets(end) {
   let index = streams.home.length - 1;
   let newEnd = index + 1;
   while(index >= end){
     let tweet = streams.home[index];
     let $tweet = $('<div class="tweet"></div>');
-    let $date = $('<div class="timestamp"></div>')
-    let date = tweet.created_at;
-    let $user = $('<span class="user"></span>')
+    let $dateRel = $('<div class="timestamp"></div>');
+    let $date = $('<div class="absolute"></div>');
+    let date = tweet.created_at.toString();
+    let dateRelative = moment(tweet.created_at).fromNow();
+    let $user = $('<span class="user"></span>');
     $user.text('@' + tweet.user);
     $date.text(date);
+    $dateRel.text(dateRelative);
+    $date.appendTo($dateRel);
     $tweet.text(': ' + tweet.message);
     $tweet.appendTo($('.tweet-container'));
-    $tweet.append($date);
+    $tweet.append($dateRel);
     $tweet.prepend($user);
     index -= 1;
   }
+  updateDates();
+  
   return newEnd;
 }
 
@@ -48,15 +66,21 @@ function generateProfile(user, start) {
   for(let i = start; i < tweets.length; i++) {
     let tweetObj = tweets[i];
     let $tweet = $('<div class="tweet"></div>');
-    let $date = $('<div class="timestamp"></div>')
-    let date = tweetObj.created_at;
+    let $dateRel = $('<div class="timestamp"></div>');
+    let $date = $('<div class="absolute"></div>');
+    let date = tweetObj.created_at.toString();
+    let dateRelative = moment(tweetObj.created_at).fromNow();
     let $user = $('<span class="user"></span>')
     $user.text(user);
     $date.text(date);
+    $dateRel.text(dateRelative);
+    $date.appendTo($dateRel);
     $tweet.text(': ' + tweetObj.message);
     $tweet.appendTo($('.profile-container'));
-    $tweet.append($date);
+    $tweet.append($dateRel);
     $tweet.prepend($user);
   }
+
+
 
 }
